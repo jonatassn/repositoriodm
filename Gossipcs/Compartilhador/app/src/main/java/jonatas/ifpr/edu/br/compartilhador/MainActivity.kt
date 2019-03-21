50a4b412fec848329caa3939d8fa6f77f3b74ec4
Josepher Castro <josephercastrosilva@gmail.com>
2019-03-21 10:24:55 -0300

    correcao intent-intent filters

--- Gossipcs/Compartilhador/app/src/main/java/jonatas/ifpr/edu/br/compartilhador/MainActivity.kt
+++ Gossipcs/Compartilhador/app/src/main/java/jonatas/ifpr/edu/br/compartilhador/MainActivity.kt
@@ -1,12 +1,32 @@
 package jonatas.ifpr.edu.br.compartilhador
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btGaleria.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 99)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val uri = data?.data
+//        Log.d("imagem",uri.toString() )
+        val intentShare = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "image/*"
+            putExtra(Intent.EXTRA_STREAM, uri)
+        }
+        startActivity(intentShare)
+
     }
 }
--- Gossipcs/Recebedor/app/src/main/java/jonatas/ifpr/edu/br/recebedor/MainActivity.kt
+++ Gossipcs/Recebedor/app/src/main/java/jonatas/ifpr/edu/br/recebedor/MainActivity.kt
@@ -1,12 +1,23 @@
 package jonatas.ifpr.edu.br.recebedor
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(intent?.action == Intent.ACTION_SEND && intent?.type == "image/*")
+            (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+                imageView.setImageURI(it)
+
+            }
+
     }
 }
